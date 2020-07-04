22edf16157af70c802433abb1139ce2ef87825d7
telecommer <tamas.z.maklary@gmail.com>
2020-07-04 14:48:57 +0200

    Parse JSON with authetication

--- get-site-json.py
+++ get-site-json.py
@@ -1,11 +1,28 @@
+
 import requests
 import json
+
+#DNA get authentication token
+url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+
+payload = {}
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='
+}
+response = requests.request("POST", url, headers=headers, data = payload)
+dna_token = json.loads(response.text.encode('utf8'))
+#print(json.dumps(dna_token, sort_keys=True, indent=1))
+token=dna_token['Token']
+#print(token)
+
+#USE to GET request
 url = "https://sandboxdnac.cisco.com/dna/intent/api/v1/site"
 
 payload = {}
 headers = {
   'Content-Type': 'application/json',
-  'X-Auth-Token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1ZTlkYmI3NzdjZDQ3ZTAwNGM2N2RkMGUiLCJhdXRoU291cmNlIjoiaW50ZXJuYWwiLCJ0ZW5hbnROYW1lIjoiVE5UMCIsInJvbGVzIjpbIjVkYzQ0NGQ1MTQ4NWM1MDA0YzBmYjIxMiJdLCJ0ZW5hbnRJZCI6IjVkYzQ0NGQzMTQ4NWM1MDA0YzBmYjIwYiIsImV4cCI6MTU5Mzg2NTU3NCwiaWF0IjoxNTkzODYxOTc0LCJqdGkiOiJmZTExZTY0Ny0zOGJiLTRjNzMtOWI0YS0wOTA4ZDE0Yzg2NTQiLCJ1c2VybmFtZSI6ImRldm5ldHVzZXIifQ.WVgl5A0tC4TwftTszGj3lechUJduh20TqAhCUCu83e1pz_T34ZeDJPvkTVB_gIO7qcNoZOX1DORhwCDjm_oPIqZmhL96VbJc0B_I0xLTNTniFn3SeCVzYNoQAXCDWCILcmwRmXjpKefSt5_g4XULHc7AxEs0wY-Hm097vJ3j9K0KP0e1KXeYVQ8RvKwQJ1-XfyxuGJDAeUA5s4jRsBh8YjgNRB1bdqMpoNbiULrjOTlljZX-WuAJU2OlpOehsnuIyeCpH2gC9NLqHCiQG5mKGDqtZLgXfE5GCSm16FkF0GsjCWr7UVPZ-fGvezm_K7Sl_o9IfCz0TigWhL2LqTtfxw'
+  'X-Auth-Token': token
 }
 
 response = requests.request("GET", url, headers=headers, data = payload)
@@ -13,7 +30,7 @@ response = requests.request("GET", url, headers=headers, data = payload)
 #print(response.text.encode('utf8'))
 json_object=json.loads(response.text.encode('utf8'))
 print(json.dumps(json_object, sort_keys=True, indent=4))
-
+#Site names on devnet DNA
 site_names = json_object['response']
 for sitename in site_names:
   print('Site Name:' + sitename['name'] + '\t site_id:' + sitename['id'])
