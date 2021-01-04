197528cb3af6a72ac2e12df9f3e2ebda2358e703
telecommer <tamas.z.maklary@gmail.com>
2021-01-04 19:43:54 +0100

    Failing is human thing

---
+++ newyear.py
@@ -0,0 +1,15 @@
+from os import system, name
+import datetime
+
+
+y = "2022-01-01 00:00:00.000001"
+z = "2022-01-01 00:00:00.000000"
+x = str(datetime.datetime.now())
+while x != y:
+    x = str(datetime.datetime.now())
+    system('clear')
+    if x > z:
+        print("Happy New Year! 2022")
+        break
+    else:
+        print(x +" Fuck 2021")
